9ec2a0cbfcb106b9dba24895798a1cd2f1a197f5
Diana P <dian6210@stud.kea.dk>
2021-10-14 19:51:36 +0200

    fetched main product image

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+async function start() {
+  let response = await fetch("sofa.svg");
+  let svgData = await response.text();
+  document.querySelector("#sofa").innerHTML = svgData;
+  userInteraction();
+}
+
+function userInteraction() {
+  document.querySelectorAll(".addColor").forEach((g) => {
+    console.log(g);
+    g.addEventListener("click", pathClicked);
+    g.addEventListener("mouseover", pathMouseover);
+    g.addEventListener("mouseout", pathMouseout);
+  });
+}
+
+function pathClicked() {
+  console.log(this);
+  this.style.fill = "grey";
+  this.style.opacity = "0.5";
+}
+
+function pathMouseover() {
+  this.style.stroke = "blue";
+}
+
+function pathMouseout() {
+  this.style.stroke = "none";
+}
