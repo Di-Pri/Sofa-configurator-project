61ae16ee3513a326e2dc2be7556d3cb000933874
Diana P <dian6210@stud.kea.dk>
2021-10-15 09:17:14 +0200

    features to product added

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ window.addEventListener("DOMContentLoaded", start);
 
 let elementToPaint;
 
+const features = {
+  glasses: false,
+};
+
 async function start() {
   let response = await fetch("sofa.svg");
   let svgData = await response.text();
   document.querySelector("#sofa").innerHTML = svgData;
+  document.querySelectorAll(".option").forEach((option) => option.addEventListener("click", toggleOption));
+
   userInteraction();
 }
 
@@ -27,12 +33,12 @@ function userInteraction() {
 function pathClicked() {
   elementToPaint = this;
   console.log(this);
-  this.style.fill = "grey";
+  this.style.fill = "black";
   this.style.opacity = "0.5";
 }
 
 function pathMouseover() {
-  this.style.stroke = "blue";
+  this.style.stroke = "#e0a148";
 }
 
 function pathMouseout() {
@@ -40,10 +46,83 @@ function pathMouseout() {
 }
 
 function colorClicked() {
-  console.log("Clickeddd", this.getAttribute("fill"));
   if (elementToPaint != undefined) {
     elementToPaint.style.fill = this.getAttribute("fill");
-  } else {
-    console.log("element not selected");
   }
+}
+
+function toggleOption(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+
+  // Toggle feature added
+  features[feature] = !features[feature];
+
+  if (features[feature]) {
+    target.classList.add("chosen");
+
+    document.querySelectorAll(`#preview [data-feature='${feature}']`).forEach((preview) => {
+      preview.classList.remove("hide");
+    });
+
+    // Created li and appended to selected ul
+    const featureElement = createFeatureElement(feature);
+    document.querySelector("#selected ul").append(featureElement);
+
+    const start = target.querySelector("img").getBoundingClientRect();
+    const end = featureElement.getBoundingClientRect();
+
+    const xdiff = start.x - end.x;
+    const ydiff = start.y - end.y;
+
+    featureElement.style.setProperty("--xdiff", xdiff);
+    featureElement.style.setProperty("--ydiff", ydiff);
+
+    featureElement.classList.add("animateIn");
+    featureElement.addEventListener("animationend", doneAnimatingIn);
+
+    function doneAnimatingIn() {
+      featureElement.classList.remove("animateIn");
+      featureElement.removeEventListener("animationend", doneAnimatingIn);
+    }
+  } else {
+    target.classList.remove("chosen");
+
+    document.querySelectorAll(`#preview [data-feature='${feature}']`).forEach((preview) => {
+      preview.classList.add("hide");
+    });
+
+    const featureElement = document.querySelector(`#selected ul [data-feature='${feature}']`);
+
+    const start = featureElement.getBoundingClientRect();
+    const end = target.querySelector("img").getBoundingClientRect();
+
+    const xdiff = end.x - start.x;
+    const ydiff = end.y - start.y;
+
+    featureElement.style.setProperty("--xdiff", xdiff);
+    featureElement.style.setProperty("--ydiff", ydiff);
+
+    featureElement.classList.add("animateOut");
+    featureElement.addEventListener("animationend", doneAnimatingOut);
+
+    function doneAnimatingOut() {
+      featureElement.classList.remove("animateOut");
+      featureElement.removeEventListener("animationend", doneAnimatingOut);
+      featureElement.remove();
+    }
+  }
+}
+
+function createFeatureElement(feature) {
+  const li = document.createElement("li");
+  li.dataset.feature = feature;
+
+  const img = document.createElement("img");
+  img.src = `images/${feature}.png`;
+  img.alt = feature;
+
+  li.append(img);
+
+  return li;
 }
